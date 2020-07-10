9b980389eddf45d870a61b014a243fb0e2ea3fe5
Glitch (hello-webpage) <none>
2020-07-10 07:19:16 +0000

    🌈📺 Checkpoint ./texto.js:18681520/424

--- texto.js
+++ texto.js
@@ -1,8 +1,8 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.login('NzMxMDI5NzY4MjExOTIyOTY1.XwgKCw.YBx6Giw5RBykgND_Qh_WdO-3oiQ');
+client.login("NzMxMDI5NzY4MjExOTIyOTY1.XwgKCw.YBx6Giw5RBykgND_Qh_WdO-3oiQ");
